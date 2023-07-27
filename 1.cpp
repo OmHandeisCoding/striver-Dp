1a21946022c5cb40f335c63c2126a7585a7ef584
Om Hande <108235406+OmHandeisCoding@users.noreply.github.com>
2023-07-27 19:03:03 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -20,6 +20,22 @@ int fib_memo(int n,vector<int>&dp)
     return dp[n]=fib_memo(n-1,dp)+fib_memo(n-2,dp);
    
 }
+//space optimized
+int fib_best(int n)
+{
+  int prev=1;
+  int prev2=0;
+  int cur;
+  for(int i=2;i<=n;i++)
+    {
+      curr=prev+prev2;
+      prev2=prev;
+      prev=curr;
+    }
+
+  return curr;
+}
+
 
 int main()
 {
