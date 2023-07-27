0251cbc705d251b48b0599657c0ba6d45020b5d2
Om Hande <108235406+OmHandeisCoding@users.noreply.github.com>
2023-07-27 19:27:23 +0530

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int f(int n)
+    {
+       int prev=1;
+       int prev2=1;
+       int curr;
+       for(int i=2;i<=n;i++)
+       {
+         curr=prev+prev2;
+         prev2=prev;
+         prev=curr;
+       }
+       if(n<=1)curr=1;
+       return curr;
+
+    }
+    int climbStairs(int n) {
+        return f(n);
+    }
+};
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    Solution obj;
+    cout<<obj.climbStairs(n);
+
+    return 0;
+}
